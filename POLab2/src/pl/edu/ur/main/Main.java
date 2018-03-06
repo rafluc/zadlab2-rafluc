67b023afddbd0ded25f6b98eb8d8879a41afbd8c
marcin <marcin@Lena>
2018-03-06 23:03:45 +0100

    init with main class

---
+++ POLab2/src/pl/edu/ur/main/Main.java
@@ -0,0 +1,16 @@
+package pl.edu.ur.main;
+
+/**
+ *
+ * @author marcin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO zad 3, 4, 6, 7
+    }
+    
+}
